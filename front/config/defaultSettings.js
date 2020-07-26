8706b2177e07032aa115bafba4c27954d5af3cd1
dongyuxinn <762491859@qq.com>
2020-07-26 23:49:09 +0800

    初始化提交前端文件

---
+++ front/config/defaultSettings.js
@@ -0,0 +1,17 @@
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '昕阳健身管理',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
