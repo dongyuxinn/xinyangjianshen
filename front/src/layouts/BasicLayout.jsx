303647763b67ce71d9f36d74c504efd30a7000bb
dongyuxinn <762491859@qq.com>
2020-07-27 00:26:32 +0800

    logo图片不好看，还是删除了吧

--- front/src/layouts/BasicLayout.jsx
+++ front/src/layouts/BasicLayout.jsx
@@ -11,7 +11,7 @@ import { Result, Button } from 'antd';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
 import { getAuthorityFromRouter } from '@/utils/utils';
-import logo from '../assets/system_icon.jpg';
+import logo from '../assets/logo.svg';
 
 const noMatch = (
   <Result
